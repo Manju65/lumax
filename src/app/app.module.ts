fee0276aed969a06d8e3b2e682a0da6f13033e15
unknown <manjunath@wimerasys.com>
2019-10-14 15:56:04 +0530

    dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PagesComponent } from './pages/pages.component';
+import { PageComponent } from './page/page.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagesComponent,
+    PageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
